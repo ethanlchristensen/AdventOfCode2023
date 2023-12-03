84dee26ed460dd05eaac5b1938530b9a93dddce9
ethanlchristensen <ethanlchristensen@outlook.com>
2023-12-03 01:18:21 -0600

    Day One - Part Two: moved to single line solution

--- dayone/dayone.py
+++ dayone/dayone.py
@@ -8,8 +8,7 @@ def part_two():
     """
     code to solve part two
     """
-    mapping={'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9'}
-    import re;return sum(list(map(lambda x:[(vals:=sorted([j for sub in[[(str(val),match.start())for match in re.finditer(key,x)]for key,val in mapping.items()if key in x]for j in sub],key=lambda item:item[1])),int(vals[0][0]+vals[-1][0])][1],open('data','r').readlines())))
+    import re;return sum(list(map(lambda x:[(vals:=sorted([j for sub in[[(str(val),match.start())for match in re.finditer(key,x)]for key,val in {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9'}.items()if key in x]for j in sub],key=lambda item:item[1])),int(vals[0][0]+vals[-1][0])][1],open('data','r').readlines())))
     
 def solve():
     """
